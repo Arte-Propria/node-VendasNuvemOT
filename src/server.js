62c1fcc584be6116c0fe9a6e8a88911666a95f45
lucasAlmeidaSilveira <lucas.asilveira.sh@gmail.com>
2023-12-05 14:19:48 -0300

    add cors

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 import express from "express"
 import dotenv from "dotenv"
+import cors from "cors"
 import productRoutes from "./routes/router.js"
 
 dotenv.config()
@@ -8,6 +9,8 @@ const app = express()
 const PORT = process.env.PORT || 3000 // Define a porta do servidor
 
 app.use(express.json())
+
+app.use(cors())
 
 app.get("/products", productRoutes)
 app.get("/orders", productRoutes)
