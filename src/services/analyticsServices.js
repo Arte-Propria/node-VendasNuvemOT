45c2b33dc4ced99c5c632f234ac45fbd6194e5f5
lucasAlmeidaSilveira <lucas.asilveira.sh@gmail.com>
2024-09-19 14:32:53 -0300

    fix sessions users

--- src/services/analyticsServices.js
+++ src/services/analyticsServices.js
@@ -64,7 +64,7 @@ export const fetchAnalytics = async ({ store, createdAtMin, createdAtMax }) => {
           { name: 'deviceCategory' },
         ],
         metrics: [
-          { name: 'engagedSessions' },
+          { name: 'sessions' },
         ],
       },
     });
