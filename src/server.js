4b6a068ccc066b94987d7167e0339ec042b20cbf
lucasAlmeidaSilveira <lucas.asilveira.sh@gmail.com>
2025-02-17 16:05:29 -0300

    add config cors test

--- src/server.js
+++ src/server.js
@@ -8,8 +8,24 @@ dotenv.config()
 const app = express()
 const PORT = process.env.PORT || 3000 // Define a porta do servidor
 
-// Configura o CORS para permitir qualquer origem
-app.use(cors({ origin: "*" }))
+const allowedOrigins = [
+	"https://vendas-nuvem-ot.vercel.app", // Seu front-end na Vercel
+	"http://localhost:8001", // Ambiente local
+	"http://localhost:5173" // Ambiente local
+]
+
+app.use(cors({
+	origin: function (origin, callback) {
+		if (!origin || allowedOrigins.includes(origin)) {
+			callback(null, true)
+		} else {
+			callback(new Error("Not allowed by CORS"))
+		}
+	},
+	credentials: true, // Permite envio de cookies/sessões se necessário
+	methods: "GET,POST,PUT,DELETE,OPTIONS", // Métodos permitidos
+	allowedHeaders: "Content-Type,Authorization" // Headers permitidos
+}))
 
 app.use(express.json())
 
