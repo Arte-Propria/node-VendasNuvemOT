b2bb04c5959829bfcd36859e81d1562453463fab
phr31 <phr31@hotmail.com>
2025-03-21 12:38:59 -0300

    Inclusão de rota e Services para dados do tiktok

--- src/controllers/tiktokControllers.js
+++ src/controllers/tiktokControllers.js
@@ -1,16 +1,25 @@
 import { fetchTiktokAuth, fetchTiktokAds } from "../services/tiktokServices.js"
 
 export const getTikTokAuth = async (req, res) => {
-	const auth = await fetchTiktokAuth()
-
-	res.redirect(auth)
+	try {
+		const auth = await fetchTiktokAuth()
+		res.redirect(auth)
+	} catch (error) {
+		console.error(error.data)
+		const status = error.status
+		res.status(status).send("Erro ao buscar a Auth do Tiktok")
+	}
 }
 
 export const getTikTokAds = async (req, res) => {
-	const { store, createdAtMin, createdAtMax } = req.params
-
-	const ads = await fetchTiktokAds(store, createdAtMin, createdAtMax)
-	
-	res.status(200).json(ads)
+	try {
+		const { store, createdAtMin, createdAtMax } = req.params
+		const ads = await fetchTiktokAds(store, createdAtMin, createdAtMax)
+		res.status(200).json(ads)
+	} catch (error) {
+		console.error(error.data)
+		const status = error.status
+		res.status(status).send("Erro ao buscar dados do Tiktok")
+	}
 }
 
--- src/services/tiktokServices.js
+++ src/services/tiktokServices.js
@@ -8,7 +8,6 @@ const appId = process.env.TIKTOK_CLIENT_APP_ID
 const appSecret = process.env.TIKTOK_CLIENT_SECRET
 const redirectUri = process.env.TIKTOK_REDIRECT_URI
 const authCode = process.env.TIKTOK_AUTH_CODE
-const accessToken = process.env.TIKTOK_ACCESS_TOKEN
 const advertiserId = process.env.TIKTOK_ADVERTISER_ID
 const adAccountIdOutlet = process.env.TIKTOK_AD_ACCOUNT_ID_OUTLET
 
@@ -22,7 +21,6 @@ export const fetchTiktokAuth = async () => {
 			grant_type: "authorization_code"
 		})
 
-		console.log(response.data)
 		if (response.data.access_token) {
 			return redirectUri + "?success=true&token=" + response.data.access_token
 		}
@@ -34,16 +32,54 @@ export const fetchTiktokAuth = async () => {
 }
 
 export const fetchTiktokAds = async (store, createdAtMin, createdAtMax) => {
+	let accessToken
+	if (store === "outlet") {
+		accessToken = process.env.TIKTOK_ACCESS_TOKEN_OT
+	} if (store === "artepropria") {
+		accessToken = process.env.TIKTOK_ACCESS_TOKEN
+	}
+
 	try {
-		const response = await axios.get("", {
+		const response = await axios.get("https://business-api.tiktok.com/open_api/v1.3/campaign/get/", {
 			headers: {
-				"Access-Token": accessToken
+				"Access-Token": accessToken // Token de acesso
+			},
+			params: {
+				advertiser_id: advertiserId, // ID do anunciante (obrigatório)
+				fields: ["campaign_id","campaign_name","budget","operation_status"], // Campos desejados
+				filter: JSON.stringify({
+					creation_filter_start_date: createdAtMin, // Filtro de data mínima (opcional)
+					creation_filter_end_date: createdAtMax // Filtro de data máxima (	opcional)
+				})
 			}
 		})
+		const dataTiktok = response.data
+		if (dataTiktok.data && dataTiktok.data.list) {
+			const adsTiktok = dataTiktok.data.list.map((ads)=>({
+				budget:ads.budget
+			}))
 
-		return response.data
+			const insightsArray = await Promise.all(adsTiktok)
+			let totalBudget = 0
+
+			insightsArray.forEach(
+				({budget}) => (
+					totalBudget += budget
+				)
+			)
+			// Retorna os valores formatados com `toFixed(2)`
+			const result = [{
+				totalCost: {
+					all: parseFloat(totalBudget.toFixed(2))
+				}
+			}]
+		
+			return result
+		} else {
+			throw new Error("Nenhum dado de campanha encontrado na resposta.")
+		}
 	} catch (error) {
-		console.error("Erro ao buscar dados do ADS:", error)
+		console.error("Erro ao buscar dados do ADS:", error.response ? error.response.data : error.message)
 		return error.message
 	}
 }
