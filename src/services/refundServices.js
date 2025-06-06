86e2c29f6ae083ff64c60217932e4a85a932cac7
phr31 <phr31@hotmail.com>
2025-06-06 15:23:25 -0300

    modificado backend para receber data do reembolso

--- src/services/refundServices.js
+++ src/services/refundServices.js
@@ -1,45 +1,51 @@
-import { query } from "../db/db.js"
+import { query } from '../db/db.js';
 
 export const fetchRefunds = async (params) => {
-	const { store, createdAtMin, createdAtMax } = params
+  const { store, createdAtMin, createdAtMax } = params;
 
-	let queryString = `SELECT * FROM reembolsos_${store} WHERE deleted = false`
-    
-	const queryParams = []
+  let queryString = `SELECT * FROM reembolsos_${store} WHERE deleted = false`;
 
-	if (createdAtMin) {
-		queryString += ` AND DATE(created_at) >= $${queryParams.length + 1}`
-		queryParams.push(new Date(createdAtMin).toISOString().split("T")[0])
-	}
+  const queryParams = [];
 
-	if (createdAtMax) {
-		queryString += ` AND DATE(created_at) <= $${queryParams.length + 1}`
-		queryParams.push(new Date(createdAtMax).toISOString().split("T")[0])
-	}
+  if (createdAtMin) {
+    queryString += ` AND DATE(created_at) >= $${queryParams.length + 1}`;
+    queryParams.push(new Date(createdAtMin).toISOString().split('T')[0]);
+  }
 
-	queryString += " ORDER BY created_at DESC"
+  if (createdAtMax) {
+    queryString += ` AND DATE(created_at) <= $${queryParams.length + 1}`;
+    queryParams.push(new Date(createdAtMax).toISOString().split('T')[0]);
+  }
 
-	const result = await query(queryString, queryParams)
-	return result.rows
-}
+  queryString += ' ORDER BY created_at DESC';
+
+  const result = await query(queryString, queryParams);
+  return result.rows;
+};
 
 export const insertRefund = async (refundData, store) => {
-	// eslint-disable-next-line camelcase
-	const { order_id, category, total, type } = refundData
-    
-	const queryString = `
+  // eslint-disable-next-line camelcase
+  const { order_id, category, total, created_at, type } = refundData;
+
+  const queryString = `
         INSERT INTO reembolsos_${store} (order_id, category, total, created_at, type)
-        VALUES ($1, $2, $3, NOW(), $4)
+        VALUES ($1, $2, $3, $4, $5)
         RETURNING *
-    `
-    
-	// eslint-disable-next-line camelcase
-	const result = await query(queryString, [order_id, category, total, type])
-	return result.rows[0]
-}
+    `;
+
+  // eslint-disable-next-line camelcase
+  const result = await query(queryString, [
+    order_id,
+    category,
+    total,
+    created_at,
+    type,
+  ]);
+  return result.rows[0];
+};
 
 export const deleteRefundById = async (id, store) => {
-	const queryString = `UPDATE reembolsos_${store} SET deleted = true WHERE id = $1 RETURNING *`
-	const result = await query(queryString, [id])
-	return result.rows[0]
-}
+  const queryString = `UPDATE reembolsos_${store} SET deleted = true WHERE id = $1 RETURNING *`;
+  const result = await query(queryString, [id]);
+  return result.rows[0];
+};
