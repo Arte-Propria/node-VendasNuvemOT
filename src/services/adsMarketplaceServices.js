84953b0e2f3bf60e9229809bdd8aa4c40c7c01c0
Lucas Almeida da Silveira <lucas.asilveira.sh@gmail.com>
2025-09-30 12:39:00 -0300

    remove duplicação do parâmetro $5 na função saveAdsMarketplace

--- src/services/adsMarketplaceServices.js
+++ src/services/adsMarketplaceServices.js
@@ -68,14 +68,13 @@ const saveAdsMarketplace = async (body) => {
       $3,
       $4,
       $5,
-      $5,
       $6,
       $7,
       $8,
       $9,
       $10,
       $11,
-			$12
+      $12
     ) RETURNING *
   `
 
