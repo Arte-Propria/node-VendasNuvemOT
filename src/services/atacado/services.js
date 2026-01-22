2915f879985f15412645d7804f9556fd068c3d3b
Lucas Almeida da Silveira <lucas.asilveira.sh@gmail.com>
2026-01-22 11:59:50 -0300

    fix fluxo atacado

--- src/services/atacado/services.js
+++ src/services/atacado/services.js
@@ -1,6 +1,6 @@
 import { saveOrderAtacado, updateOrderAtacadoStatus } from "../../db/saveOrder.js"
 import { logAtacado } from "../../utils/logger.js"
-import { getProductDetails } from "../../utils/tiny"
+import { getProductDetails } from "../../utils/tiny.js"
 
 const atacadoNames = [
 	"leroymerlin",
